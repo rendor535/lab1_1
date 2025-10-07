84d74e97d071f28f3142869c976eb402775d5d6a
Marcin Wiśniowski <149424888+rendor535@users.noreply.github.com>
2025-10-07 09:15:35 +0000

    Com1_1.1

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-(function () {
-  //TODO
+(function() {
+
+  var ex1_button = document.getElementById('ex1_button');
+  var ex1_content = document.getElementById('ex1_content');
+  //Ex1
+  ex1_button.onclick = function() {
+    var tabela = []
+    for (var i = 0; i <= 9; i++) {
+      tabela.push(i);
+    }
+    ex1_content.innerHTML = tabela.toString();
+  }
 })();
