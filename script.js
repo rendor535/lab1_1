5ad8c8dd4ef85b532141528ab3d6f2808f0dcf49
Marcin Wiśniowski <149424888+rendor535@users.noreply.github.com>
2025-10-07 09:40:00 +0000

    Com1_1.2

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
 (function() {
+  document.getElementById("ex2_text").addEventListener("input", number);
+  function number() {
+    const correct = "Numer telefonu nie jest poprawny"
+    var number = document.getElementById("ex2_text");
+    const log = document.getElementById('ex2_content');
+    const log3 = document.getElementById('ex3_content');
+    const log4 = document.getElementById('ex4_content');
+    const log5 = document.getElementById('ex5_content');
 
-  var ex1_button = document.getElementById('ex1_button');
-  var ex1_content = document.getElementById('ex1_content');
-  //Ex1
-  ex1_button.onclick = function() {
-    var tabela = []
-    for (var i = 0; i <= 9; i++) {
-      tabela.push(i);
+    if (number.value.match(/[\W]/)) {
+      log5.textContent = "Numer nie może zawierać znaków specjalnych 🤓👆";
     }
-    ex1_content.innerHTML = tabela.toString();
+    else {
+      log5.textContent = " ";
+    }
+
+    if (number.value.match(/[a-zA-Z]/)) {
+      log4.textContent = "Numer nie może zawierać liter >:(";
+    }
+    else {
+      log4.textContent = " ";
+    }
+
+    if (number.value.length != 9) {
+      log3.textContent = "Długość numeru musi być równa 9 😡😡😡";
+    }
+    else {
+      log3.textContent = " ";
+    }
+
+    if (number.value.match(/^[0-9]+$/) && number.value.length == 9) {
+      log.textContent = "Numer telefonu jest poprawny ";
+    }
+    else {
+      log.textContent = " ";
+    }
   }
 })();
